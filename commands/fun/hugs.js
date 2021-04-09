ce2ac14e5babdf237473b6f3a1068b9f3f11d983
theticarcher38 <hunterleegoodman@gmail.com>
2021-04-09 06:32:45 -0500

    UPDATED hug command

--- commands/fun/hugs.js
+++ commands/fun/hugs.js
@@ -3,15 +3,9 @@ module.exports = {
     name: 'hug',
     description: 'Send a hug to mentioned user',
     run: async(client, message, args) => {
-        const huggingGif = [
-            'https://media.giphy.com/media/VbawWIGNtKYwOFXF7U/giphy.gif', 
-            'https://media.giphy.com/media/3oEdv4hwWTzBhWvaU0/giphy.gif', 
-            'https://media.giphy.com/media/3M4NpbLCTxBqU/giphy.gif', 
-            'https://media.giphy.com/media/yidUzriaAGJbsxt58k/giphy.gif',
-            'https://media.giphy.com/media/ZBQhoZC0nqknSviPqT/giphy.gif'
-        ]
+        const huggingGif = require('../../assets/gifs.json');
         const user = message.mentions.users.first() || client.users.cache.get(u => u.id === args[0]);
-        const gif = huggingGif[Math.floor(Math.random() * huggingGif.length)]
+        const gif = huggingGif.huggingGif[Math.floor(Math.random() * huggingGif.huggingGif.length)]
 
         if (!user) {
             message.channel.send('Please mention a user to hug');
